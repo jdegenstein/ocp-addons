d73a7ed418d6bc8c700087c22330bc90c0542b7c
Bernhard <b_walter@arcor.de>
2025-10-06 18:24:34 +0200

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 description = "Addon packages for OCP"
 
 occ_libs = [
